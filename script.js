10c573e7f3fd4df48dbbc662b6e55ac060f756e4
CodyPal <38052829+CodyPal@users.noreply.github.com>
2021-02-08 11:20:21 -0800

    final date format

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const d = new Date();
 const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
 const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
 const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
-todaysDate.textContent = `${da}-${mo}-${ye}`;
+todaysDate.textContent = `${mo}-${da}-${ye}`;
 
 // Update DOM function
 function updateDOM(){
